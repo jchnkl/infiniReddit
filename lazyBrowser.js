8cd524bf158c9423f77a6a0368ccc2760661ea1e
Jochen Keil <jochen.keil@gmail.com>
2015-01-26 22:34:32 +0100

    Clean up code

--- lazyBrowser.js
+++ lazyBrowser.js
@@ -10,9 +10,7 @@ var history = null;
 // container for appending the reddit listings
 var container = null;
 
-var nitems = 2;
-var nthItem = 3;
-
+// are new listings completely appended to the dom?
 var isReady = true;
 
 // clear container
@@ -20,60 +18,6 @@ function clearNode(node)
 {
   while (node.firstChild) {
     node.removeChild(node.firstChild);
-  }
-}
-
-/*
-function appendListing(result)
-{
-  var data = result.data.children[0].data;
-  var thumbnail = document.createElement("img");
-  var img = document.createElement("img");
-  var row = document.createElement("div");
-  var left = document.createElement("div");
-  var right = document.createElement("div");
-  var link = document.createElement("a");
-
-  // row.className = "row";
-  // left.className = "col-xs-4";
-  // thumbnail.setAttribute("src", data.thumbnail);
-
-  link.setAttribute("href", "http://www.reddit.com" + data.permalink);
-  link.innerHTML = data.title;
-  link.className = "center";
-  row.appendChild(link);
-
-  // right.className = "col-xs-8";
-  // img.className = "img-responsive";
-  // img.setAttribute("src", data.url);
-  // right.appendChild(img);
-  // row.appendChild(right);
-
-  img.className = "center";
-  img.setAttribute("src", data.url);
-  row.appendChild(img);
-
-  container.innerHTML = "";
-  container.appendChild(row);
-}
-*/
-
-function appendListings(result)
-{
-  container.innerHTML = "";
-  for (var i = 0; i < result.data.children.length; ++i) {
-    appendListing(result.data.children[i].data);
-  }
-}
-
-function appendMoreListings()
-{
-  ++nthItem;
-  var children = history.peek().data.children;
-  if (nthItem < children.length) {
-    appendListing(children[nthItem].data);
-  } else {
-    --nthItem;
   }
 }
 
@@ -107,10 +51,7 @@ function loadNextListing()
   if (history.isEmpty()) {
     promise = reddit(url).get({ limit: 16 });
   } else {
-    // var last = history.peek().data.children.length - 1;
-    // var name = history.peek().data.children[last].data.name;
     var name = history.peek().name;
-    // alert(name);
     promise = reddit(url).get({ limit: 16, after: name });
   }
 
@@ -130,7 +71,6 @@ function loadNextListing()
 
     for (var i = 0; i < result.data.children.length; ++i) {
       appendListing(result.data.children[i].data);
-      // alert("data.name: " + result.data.children[i].data.name);
     }
 
     for (var i = 0; i < result.data.children.length; ++i) {
@@ -147,135 +87,6 @@ function loadNextListing()
   });
 }
 
-function updateListing(next)
-{
-  clearNode(container);
-  container.innerHTML = "Loading ...";
-
-  history.clear();
-
-  if (next) {
-    var promise = null;
-    promise = reddit(url).get({ limit: 20 });
-
-    // if (history.isEmpty()) {
-    //   promise = reddit(url).get({ limit: 1 });
-    // } else {
-    //   var name = history.peek().data.children[0].data.name;
-    //   promise = reddit(url).get({ limit: 1, after: name });
-    // }
-
-    promise.then(function(result)
-    {
-      appendListing(result);
-      return result;
-    }).then(function(result)
-    {
-      history.push(result);
-      return result;
-    }).then(function(result)
-    {
-      updatePrev();
-    }).catch(function(error)
-    {
-      div = document.createElement("div");
-      div.innerHTML = "ERROR: " + error;
-      container.appendChild(div);
-    });
-
-  } else {
-    history.pop();
-    appendListing(history.peek());
-    updatePrev();
-  }
-}
-
-/*
-function updateListing(next)
-{
-  clear(container);
-  container.innerHTML = "Loading ...";
-
-  if (next) {
-    var promise = null;
-
-    if (history.isEmpty()) {
-      promise = reddit(url).get({ limit: 1 });
-    } else {
-      var name = history.peek().data.children[0].data.name;
-      promise = reddit(url).get({ limit: 1, after: name });
-    }
-
-    promise.then(function(result)
-    {
-      appendListing(result);
-      return result;
-    }).then(function(result)
-    {
-      history.push(result);
-      return result;
-    }).then(function(result)
-    {
-      updatePrev();
-    }).catch(function(error)
-    {
-      div = document.createElement("div");
-      div.innerHTML = "ERROR: " + error;
-      container.appendChild(div);
-    });
-
-  } else {
-    history.pop();
-    appendListing(history.peek());
-    updatePrev();
-  }
-}
-*/
-
-function updatePrev()
-{
-  var prev_li = document.getElementById("prev-li");
-  var prev_a = document.getElementById("prev-a");
-
-  if (history.size() == 1) {
-    prev_li.className = "previous disabled";
-    prev_a.setAttribute("onClick", "");
-  } else {
-    prev_li.className = "previous";
-    prev_a.setAttribute("onClick", "updateListing(false);");
-  }
-}
-
-function more()
-{
-  ++nitems;
-  updateListing(true);
-}
-
-function less()
-{
-  --nitems;
-  updateListing(true);
-}
-
-function info()
-{
-  // alert("More loaded" + " " + window.innerHeight
-  //                     + " " + window.innerHeight
-  //                     + " " + window.pageYOffset
-  //                     + " " + window.clientHeight
-  //                     + " " + document.documentElement.clientHeight
-  //                     + " " + document.documentElement.scrollTop
-  //                     + " " + window.document.documentElement.scrollHeight
-  //                     + " " + window.document.documentElement.scrollTop
-  //                     + " " + window.document.documentElement.scrollTopMax
-  //     );
-  alert("More loaded" + " " + document.documentElement.scrollTop
-                      + " " + document.documentElement.scrollTopMax
-      );
-  // alert("Info: " + " " + window.location);
-}
-
 function loadMore()
 {
   window.onscroll = null;
@@ -284,22 +95,12 @@ function loadMore()
       scrollTop    = document.documentElement.scrollTop;
       scrollTopMax = document.documentElement.scrollTopMax;
 
-  // if (isReady && scrollTop + barrier > scrollTopMax) {
   if (isReady && scrollTopMax / scrollTop < barrier) {
-    // info();
     loadNextListing();
   }
 
   window.onscroll = loadMore;
 }
-
-// function bindScroll()
-// {
-//   if (true) {
-//     window.onscroll = null;
-//     loadMore();
-//   }
-// }
 
 function main()
 {
@@ -308,17 +109,8 @@ function main()
   history = new Stack();
   container = document.getElementById("container");
   reddit = new window.Snoocore({ userAgent: 'LazyBrowser@0.0.1 by jrk-' });
-  // updateListing(true);
-
-  // loadMore();
-  // window.onscroll = bindScroll;
-
-  // var barrier      = 0.95 * document.documentElement.clientHeight
-  //     scrollTop    = document.documentElement.scrollTop;
-  //     scrollTopMax = document.documentElement.scrollTopMax;
 
   loadNextListing();
-
   window.onscroll = loadMore;
 }
 
